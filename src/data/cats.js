ac3ba275fd8fa9f1cccaf49fc220993610ca4dc9
bundi <robermagara66@gmail.com>
2020-03-30 17:19:13 +0300

    Pet adoption application first commit

---
+++ src/data/cats.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    name: 'Roger',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'gray',
+    weight: 15,
+    location: 'threed',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Henry',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'orange/white',
+    weight: 17,
+    location: 'threed',
+    notes: 'Super friendly.'
+  },
+  {
+    name: 'Fish',
+    breed: 'tuxedo',
+    gender: 'male',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweet kitty. He loves getting his belly rubbed.'
+  }
+
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Zeus',
+    breed: 'afghan hound',
+    gender: 'male',
+    age: 9,
+    color: 'gray',
+    weight: 68,
+    location: 'threed',
+    notes: 'Jovial.'
+  },
+  {
+    name: 'Hillary',
+    breed: 'mut',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'threed',
+    notes: 'Super friendly.'
+  }
+
+]
